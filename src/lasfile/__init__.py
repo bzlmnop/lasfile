9666d0a39d9859c1f75ab5ae7ec422887789d206
bzlmnop <62496848+bzlmnop@users.noreply.github.com>
2023-08-06 01:26:25 -0500

    Update __init__.py

--- src/lasfile/__init__.py
+++ src/lasfile/__init__.py
@@ -2,5 +2,6 @@ from .lasfile import LASFile                          # noqa: F401, E261
 from .lasfile import LASSection                       # noqa: F401, E261
 from .lasfile import LASData                          # noqa: F401, E261
 from .lasfile import read                             # noqa: F401, E261
+from .lasfile import write                            # noqa: F401, E261
 from .lasfile import api_from_las                     # noqa: F401, E261
 from .lasfile import error_check                      # noqa: F401, E261
